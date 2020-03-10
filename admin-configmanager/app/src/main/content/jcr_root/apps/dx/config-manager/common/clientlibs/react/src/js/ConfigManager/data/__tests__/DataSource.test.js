ddfe8c89dbb2daa996e3290ca7847938a630b428
Chris Peyer <cpeyer@adobe.com>
2020-03-10 15:04:23 -0700

    Increase coverage of 2 inital tests to 100%

--- admin-configmanager/app/src/main/content/jcr_root/apps/dx/config-manager/common/clientlibs/react/src/js/ConfigManager/data/__tests__/DataSource.test.js
+++ admin-configmanager/app/src/main/content/jcr_root/apps/dx/config-manager/common/clientlibs/react/src/js/ConfigManager/data/__tests__/DataSource.test.js
@@ -21,10 +21,69 @@ window.fetch = jest.fn().mockImplementation((path) => Promise.resolve({
 }));
 
 describe('Datasource', () => {
+    afterEach(() => {
+        window.fetch.mockClear();
+    });
+
     test('should fetch datasourcepath with getTree', async () => {
         const cds = new DataSource('path/to/datasource');
         const items = await cds.getTree();
         expect(items).toEqual(['one', 'two']);
+        expect(window.fetch).toHaveBeenCalledTimes(1);
         expect(window.fetch).toHaveBeenCalledWith('path/to/datasource.model.json');
+    });
+
+    test('getChildren should use getTree when not passed any arguments ', async () => {
+        const cds = new DataSource('path/to/datasource');
+        const items = await cds.getChildren();
+        expect(items).toEqual(['one', 'two']);
+        expect(window.fetch).toHaveBeenCalledTimes(1);
+        expect(window.fetch).toHaveBeenCalledWith('path/to/datasource.model.json');
+    });
+
+    test('getChildren should return items.children when items arg is passed', async () => {
+        const cds = new DataSource('path/to/datasource');
+        const item = { children: 'over here' };
+        const result = await cds.getChildren(item);
+        expect(result).toBe('over here');
+    });
+
+    test('hasChildren should return a boolean indicating if item has children', async () => {
+        const cds = new DataSource('path/to/datasource');
+        const item1 = { children: 'over here' };
+        const result1 = await cds.hasChildren(item1);
+        expect(result1).toStrictEqual(true);
+
+        const item2 = { notchildren: 'over here' };
+        const result2 = await cds.hasChildren(item2);
+        expect(result2).toStrictEqual(false);
+    });
+
+    test('isItemEqual should return item.label equality', () => {
+        const cds = new DataSource('path/to/datasource');
+        const item1 = { label: 'labelOne' };
+        const itemOne = { label: 'labelOne' };
+        const item2 = { label: 'labelTwo' };
+
+        expect(cds.isItemEqual(item1, itemOne)).toStrictEqual(true);
+        expect(cds.isItemEqual(item1, item2)).toStrictEqual(false);
+    });
+
+    test('getTree should output console error when fetch fails', async () => {
+        global.console = { log: jest.fn() };
+        window.fetch = jest.fn().mockImplementation((path) => new Promise((resolve, reject) => {
+            reject('fetch error');
+        }));
+
+        const cds = new DataSource('path/to/datasource');
+        try {
+            const items = await cds.getTree();
+        } catch (e) {
+            expect(e.toString()).toBe('TypeError: Cannot read property \'items\' of undefined');
+        }
+        expect(window.fetch).toHaveBeenCalledTimes(1);
+        expect(window.fetch).toHaveBeenCalledWith('path/to/datasource.model.json');
+        expect(global.console.log).toHaveBeenCalledTimes(1);
+        expect(global.console.log.mock.calls[0]).toEqual(['Error: ','fetch error']);
     });
 });
