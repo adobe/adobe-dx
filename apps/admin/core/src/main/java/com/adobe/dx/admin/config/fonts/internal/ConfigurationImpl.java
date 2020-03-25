e45b3af42a183c2ee725741e71b5068a2d741ca5
Nicolas Peltier <npeltier@adobe.com>
2020-03-25 15:31:13 +0100

    Introduce ContextAwareDatasource

--- apps/admin/core/src/main/java/com/adobe/dx/admin/config/fonts/internal/ConfigurationImpl.java
+++ apps/admin/core/src/main/java/com/adobe/dx/admin/config/fonts/internal/ConfigurationImpl.java
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-package com.adobe.dx.admin.config.fonts.impl;
+package com.adobe.dx.admin.config.fonts.internal;
 
 import java.util.Arrays;
 import java.util.Calendar;
