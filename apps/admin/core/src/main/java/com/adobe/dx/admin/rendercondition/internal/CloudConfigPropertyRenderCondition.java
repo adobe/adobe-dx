fef8eff509cda3c40e02a2b3b9b49127ce58b72e
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2021-05-18 13:33:32 +0200

    space between package and copyright comment.

--- apps/admin/core/src/main/java/com/adobe/dx/admin/rendercondition/internal/CloudConfigPropertyRenderCondition.java
+++ apps/admin/core/src/main/java/com/adobe/dx/admin/rendercondition/internal/CloudConfigPropertyRenderCondition.java
@@ -13,6 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+ 
 package com.adobe.dx.admin.rendercondition.internal;
 
 import static org.apache.sling.api.servlets.HttpConstants.METHOD_GET;
--- apps/admin/core/src/test/java/com/adobe/dx/admin/rendercondition/internal/CloudConfigPropertyRenderConditionTest.java
+++ apps/admin/core/src/test/java/com/adobe/dx/admin/rendercondition/internal/CloudConfigPropertyRenderConditionTest.java
@@ -1,5 +1,5 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- ~ Copyright 2020 Adobe
+ ~ Copyright 2021 Adobe
  ~
  ~ Licensed under the Apache License, Version 2.0 (the "License");
  ~ you may not use this file except in compliance with the License.
@@ -13,7 +13,8 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-package com.adobe.dx.admin.rendercondition.internal;
+ 
+ package com.adobe.dx.admin.rendercondition.internal;
 
 import static org.apache.jackrabbit.JcrConstants.JCR_CONTENT;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
